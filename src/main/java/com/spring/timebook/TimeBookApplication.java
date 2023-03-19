9d618569a4c5e904da57627ac0d1f8084e7b04a9
easymean <m2162003@naver.com>
2023-03-19 17:29:50 +0900

    first commit

---
+++ src/main/java/com/spring/timebook/TimeBookApplication.java
@@ -0,0 +1,13 @@
+package com.spring.timebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/timebook/TimeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.timebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
