929900ba54bb2459353ac696eed4b7c61ff3089b
easymean <easymean@ncsoft.com>
2023-05-15 17:42:43 +0900

    spring data JPA 메서드 수정

--- src/main/java/com/spring/timebook/tag/TagRepository.java
+++ src/main/java/com/spring/timebook/tag/TagRepository.java
@@ -4,7 +4,5 @@ import java.util.List;
 
 public interface TagRepository {
     Tag save(Tag tag);
-    List<Tag> findAll(SpendingType type);
-    Tag update(Tag tag);
-    void delete(Long id);
+    List<Tag> findAllBySpendingType(SpendingType type);
 }
