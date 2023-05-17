68cde92ce882957372ce98d1e68b1e4d01ea1450
easymean <easymean@ncsoft.com>
2023-05-17 16:33:28 +0900

    interceptor 초기화

---
+++ src/main/java/com/spring/timebook/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.spring.timebook.config;
+
+import com.spring.timebook.aop.TokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new TokenInterceptor());
+    }
+}
