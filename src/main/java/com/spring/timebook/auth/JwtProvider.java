b2358cc86c2cd21df867ebeebaf7c69b90a8a7c3
easymean <easymean@ncsoft.com>
2023-05-19 12:57:30 +0900

    토큰 파싱 후 유저 id를 가져오는 로직 추가 다른 로그인 방식에서도 유저 id만 저장하는 형식을 취한다.

--- src/main/java/com/spring/timebook/auth/JwtProvider.java
+++ src/main/java/com/spring/timebook/auth/JwtProvider.java
@@ -1,17 +1,14 @@
 package com.spring.timebook.auth;
 
 import com.spring.timebook.user.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 
 import java.util.Date;
 
 public class JwtProvider implements TokenProvider {
 
     private static final String AUTHORITIES_KEY= "username";
-    private static final long JWT_EXPIRATION_MS = 604800000;
+    private static final long JWT_EXPIRATION_MS = 24 * 60 * 60 * 1000L;
     private final byte[] key;
 
 
@@ -26,7 +23,7 @@ public class JwtProvider implements TokenProvider {
         Date validity = new Date(now + JWT_EXPIRATION_MS);
 
         return Jwts.builder()
-                .setSubject(user.getId().toString())
+                .setSubject(user.getId().toString()) // 구분자
                 .claim(AUTHORITIES_KEY, user.getUsername())
                 .signWith(SignatureAlgorithm.HS256, key)
                 .setExpiration(validity)
@@ -34,20 +31,34 @@ public class JwtProvider implements TokenProvider {
     }
 
     @Override
-    public boolean validateToken(String token) {
+    public String getUserIdFromToken(String token){
+        if(validateToken(token)){
+            return parseClaims(token).getSubject();
+        }else {
+            throw new JwtException("Can not get userId from token");
+        }
+    }
+
+    private boolean validateToken(String token) {
         try {
-            Claims claims = Jwts.parser().setSigningKey(key)
-                    .parseClaimsJws(token)
-                    .getBody();
+            Jwts.parser().setSigningKey(key)
+                    .parseClaimsJws(token);
             return true;
-        }catch(ExpiredJwtException e){
+        } catch(ExpiredJwtException e){
             // 에러 처리
+            // 토큰이 만료되었습니다.
             e.printStackTrace();
-        }catch (Exception e){
+        } catch(UnsupportedJwtException | MalformedJwtException | SignatureException | IllegalArgumentException e){
             // 에러 처리
+            // 유효하지 않은 토큰입니다.
             e.printStackTrace();
         }
-
         return false;
+    }
+
+    private Claims parseClaims(String token){
+        return Jwts.parser().setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
--- src/main/java/com/spring/timebook/auth/TokenProvider.java
+++ src/main/java/com/spring/timebook/auth/TokenProvider.java
@@ -3,7 +3,6 @@ package com.spring.timebook.auth;
 import com.spring.timebook.user.User;
 
 public interface TokenProvider {
-
     String createToken(User user);
-    boolean validateToken(String token);
+    String getUserIdFromToken(String token);
 }
