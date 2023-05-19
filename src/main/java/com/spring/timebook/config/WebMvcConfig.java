5b8f4c37e135d7baaf45e60025e2ea06fd45e6fb
easymean <easymean@ncsoft.com>
2023-05-19 12:58:26 +0900

    interceptor와 빈 등록

--- src/main/java/com/spring/timebook/config/WebMvcConfig.java
+++ src/main/java/com/spring/timebook/config/WebMvcConfig.java
@@ -1,6 +1,10 @@
 package com.spring.timebook.config;
 
-import com.spring.timebook.aop.TokenInterceptor;
+import com.spring.timebook.aop.AuthenticationInterceptor;
+import com.spring.timebook.auth.JwtProvider;
+import com.spring.timebook.auth.TokenProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,8 +12,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Value("{jwt.secret}")
+    private String tokenSecret;
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+    public WebMvcConfig(AuthenticationInterceptor authenticationInterceptor) {
+        this.authenticationInterceptor = authenticationInterceptor;
+    }
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new TokenInterceptor());
+        registry.addInterceptor(authenticationInterceptor);
+    }
+
+    @Bean
+    public TokenProvider tokenProvider(){
+        return new JwtProvider(tokenSecret);
     }
 }
